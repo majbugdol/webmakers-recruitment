a63511ac3b79c3a6cef545f73be634fa66fbdba5
majbugdol <majbugdol@gmail.com>
2022-09-06 17:00:15 +0200

    add weather-map component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherMapComponent } from './weather-map/weather-map.component';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, WeatherMapComponent],
+  imports: [BrowserModule, AppRoutingModule, GoogleMapsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/weather-map/weather-map.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-map',
+  templateUrl: './weather-map.component.html',
+  styleUrls: ['./weather-map.component.scss'],
+})
+export class WeatherMapComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
