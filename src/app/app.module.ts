d806d90165bcd1c567421a618b5eba9cd94d0d7b
majbugdol <majbugdol@gmail.com>
2022-09-06 18:38:31 +0200

    add open-weather service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherMapComponent } from './weather-map/weather-map.component';
-import { GoogleMapsModule } from '@angular/google-maps';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [AppComponent, WeatherMapComponent],
-  imports: [BrowserModule, AppRoutingModule, GoogleMapsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBD9_pIoy2foIRW4EPhe0af6UJaN050y_c',
+      libraries: ['places'],
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/weather-map/weather-map.component.ts
+++ src/app/weather-map/weather-map.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { MapsAPILoader } from '@agm/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-weather-map',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-map.component.scss'],
 })
 export class WeatherMapComponent implements OnInit {
-  constructor() {}
+  lat: number = 50.265252093435215;
+  lng: number = 19.02180235179187;
 
-  ngOnInit(): void {}
+  constructor(private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) {}
+
+  ngOnInit(): void {
+    this.mapsAPILoader.load().then(() => {});
+  }
+
+  onMapClick(event: any) {
+    console.table(event.coords);
+    this.lat = event.coords.lat;
+    this.lng = event.coords.lng;
+  }
 }
